3f3cca710d85e75490c73f7eaac4b4f65f4e0c3c
孔冬冬 <kongdd@mail2.sysu.edu.cn>
2017-09-21 21:16:50 +0800

    modified to surport no brackets split

--- split_line.py
+++ split_line.py
@@ -1,4 +1,6 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+
 
 class SplitLineCommand(sublime_plugin.TextCommand):
     def run(self, edit, **args):
@@ -9,12 +11,6 @@ class SplitLineCommand(sublime_plugin.TextCommand):
             settings = sublime.load_settings('Split_Line.sublime-settings')
 
             original_text = selected_text
-
-            num_tabs = int(int(indent_size)/4)
-
-            remainder = int(indent_size - 4*num_tabs)
-            indent = "\t" * num_tabs + ' ' * remainder
-
             selected_text = selected_text.lstrip(' ').lstrip('\t')
 
             pre_multi = ''
@@ -33,45 +29,49 @@ class SplitLineCommand(sublime_plugin.TextCommand):
 
             brackets = sorted(brackets, key=lambda tup: tup[0])
 
-            if brackets[0][1] == '(':
-                lbracket = '('
-                rbracket = ')'
-            elif brackets[0][1] == '[':
-                lbracket = '['
-                rbracket = ']'
-            elif brackets[0][1] == '{':
-                lbracket = '{'
-                rbracket = '}'
-
             lb_loc = None
             rb_loc = None
+            IN_BRACKET = True
+            if len(brackets) == 0:
+                IN_BRACKET = False
+            else:
+                if brackets[0][1] == '(':
+                    lbracket = '('
+                    rbracket = ')'
+                elif brackets[0][1] == '[':
+                    lbracket = '['
+                    rbracket = ']'
+                elif brackets[0][1] == '{':
+                    lbracket = '{'
+                    rbracket = '}'
+
+                # then reselect text
+                if lbracket in selected_text:
+                    lb_loc = selected_text.index(lbracket)
+                    pre_multi = selected_text[:lb_loc + 1]
+                    selected_text = selected_text[lb_loc + 1:]
+
+                if rbracket in selected_text:
+                    rb_loc = selected_text.rfind(rbracket)
+
+                    if selected_text[rb_loc - 1] == ',':
+                        trailing_comma = True
+
+                    post_multi = selected_text[rb_loc:]
+                    selected_text = selected_text[:rb_loc]
+
+            num_tabs = int(int(indent_size) / 4)
+            remainder = int(indent_size - 4 * num_tabs)
+            indent = "\t" * num_tabs + ' ' * remainder
 
             trailing_comma = False
-
-            if lbracket in selected_text:
-                lb_loc = selected_text.index(lbracket)
-                pre_multi = selected_text[:lb_loc+1]
-                selected_text = selected_text[lb_loc+1:]
-
-            if rbracket in selected_text:
-                rb_loc = selected_text.rfind(rbracket)
-
-                if selected_text[rb_loc-1] == ',':
-                    trailing_comma = True
-
-                post_multi = selected_text[rb_loc:]
-                selected_text = selected_text[:rb_loc]
-
             multi_line_text = None
 
+            print(selected_text)
             if "\n" not in selected_text:
-                if (
-                    (
-                        ',' in selected_text.strip(' ')
-                        and selected_text.strip(' ').index(',') != len(selected_text.strip(' '))-1
-                    )
-                    or (lb_loc is not None and rb_loc is not None)
-                ):
+                if ((',' in selected_text.strip(' ')
+                        and selected_text.strip(' ').index(',') != len(selected_text.strip(' ')) - 1)
+                        or (lb_loc is not None and rb_loc is not None)):
 
                     tarray = []
                     l1 = 0
@@ -85,9 +85,9 @@ class SplitLineCommand(sublime_plugin.TextCommand):
 
                         if c == ',' and not in_bracket:
                             tarray.append(selected_text[l1:i].strip(' '))
-                            l1 = i+1
-                        elif i == len(selected_text)-1:
-                            tarray.append(selected_text[l1:i+1].strip(' '))
+                            l1 = i + 1
+                        elif i == len(selected_text) - 1:
+                            tarray.append(selected_text[l1:i + 1].strip(' '))
 
                     # Call iteratively on tarray memebers
                     if settings.get('recursive_split', False):
@@ -96,23 +96,33 @@ class SplitLineCommand(sublime_plugin.TextCommand):
                                 ('(' in t or '[' in t or '{' in t)
                                 and remove_ws(t) not in ['(', ')', '[', ']', '{', '}']
                             ):
-                                _, tarray[i:i+1] = split_text(t, indent_size)
+                                _, tarray[i:i + 1] = split_text(t, indent_size)
 
-                    full_array = [pre_multi]
-                    full_array.extend(["\t"+t for t in tarray])
-                    full_array.append(post_multi)
+                    if IN_BRACKET:
+                        sep = "\t"
+                        full_array = [pre_multi]
+                        full_array.extend(["\t" + t for t in tarray])
+                        full_array.append(post_multi)
+                        multi_line_text = indent + pre_multi + '\n'
+                    else:
+                        sep = ""
+                        full_array = tarray
+                        multi_line_text = ""
+                    # print('full_array', full_array)
+                    # print('pre_multi', pre_multi, 'tarray', tarray, 'post_multi', post_multi)
 
-                    multi_line_text = indent + pre_multi + '\n'
                     for i, l in enumerate(tarray):
                         if (
-                            (i != len(tarray)-1 or trailing_comma)
+                            (i != len(tarray) - 1 or trailing_comma)
                             and remove_ws(l) not in ['(', '[', '{']
                         ):
-                            multi_line_text += indent + "\t" + l + ',\n'
+                            multi_line_text += indent + sep + l + ',\n'
                         else:
-                            multi_line_text += indent + "\t" + l + '\n'
+                            multi_line_text += indent + sep + l + '\n'
 
                     multi_line_text += indent + post_multi
+
+                    print('multi_line_text', multi_line_text)
 
                     return multi_line_text, full_array
                 else:
@@ -127,14 +137,14 @@ class SplitLineCommand(sublime_plugin.TextCommand):
                 line = self.view.line(region)
                 selected_text = self.view.substr(line)
 
-                indent_text = selected_text[:len(selected_text) - len(selected_text.lstrip(' ').lstrip('\t').lstrip(' '))]
+                indent_text = selected_text[:len(
+                    selected_text) - len(selected_text.lstrip(' ').lstrip('\t').lstrip(' '))]
                 indent_size = 0
                 for c in indent_text:
                     if c == '\t':
                         indent_size += 4
                     else:
                         indent_size += 1
-
+                # print(indent_size)
                 multi_line_text, _ = split_text(selected_text, indent_size)
-
                 self.view.replace(edit, line, multi_line_text)
